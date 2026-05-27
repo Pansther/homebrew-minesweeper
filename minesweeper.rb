e3e1089cd390c3aef4271a32f32b2e86d70ed9a2
Weerawit Sirakunwat <p_weerawit@global.tencent.com>
2026-05-27 09:20:07 +0700

    feat: Upgrade to v0.3.0

--- minesweeper.rb
+++ minesweeper.rb
@@ -1,17 +1,17 @@
 class Minesweeper < Formula
   desc "Vim-friendly Minesweeper TUI"
   homepage "https://github.com/Pansther/minesweeper-tui"
-  version "0.1.0"
+	version "0.3.0"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/Pansther/minesweeper-tui/releases/download/v0.1.0/minesweeper-tui-darwin-x64.tar.gz"
-    sha256 "fb4c1a7ccf09b35c1a226ef353294a55a429fc72d0497005956a90b84090957c"
+		url "https://github.com/Pansther/minesweeper-tui/releases/download/v0.3.0/minesweeper-tui-darwin-x64.tar.gz"
+    sha256 "30ab45ff0ea23efe8f4601aa99851713103d7f4fdadd7454e8d0fe4e4444f65a"
   elsif OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/Pansther/minesweeper-tui/releases/download/v0.1.0/minesweeper-tui-darwin-arm64.tar.gz"
-    sha256 "1bfa33370ccdb8dbcae66c906baaae695f4aed83a76891b39ffe277dad8e82fb"
+		url "https://github.com/Pansther/minesweeper-tui/releases/download/v0.3.0/minesweeper-tui-darwin-arm64.tar.gz"
+    sha256 "e8547f3a05f165b314222e26a1536e74209aa8c9708b1a7d1e6744cbfa8ef319"
   elsif OS.linux?
-    url "https://github.com/Pansther/minesweeper-tui/releases/download/v0.1.0/minesweeper-tui-linux-x64.tar.gz"
-    sha256 "679292892f6008ee570deecd937f4f35f8d78c2eec619b56a66c239c9b6e118c"
+		url "https://github.com/Pansther/minesweeper-tui/releases/download/v0.3.0/minesweeper-tui-linux-x64.tar.gz"
+    sha256 "5eb75032323ddcbb6255c283ba01c2e1468ec815e5590331e3439e52bf91c422"
   end
 
   def install
@@ -25,6 +25,6 @@ class Minesweeper < Formula
   end
 
   test do
-    assert_match "0.1.0", shell_output("#{bin}/minesweeper --version")
+		assert_match "0.3.0", shell_output("#{bin}/minesweeper --version")
   end
 end
